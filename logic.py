7dad3f9817d2d9648d1bfba215fe9a0fe3b63b89
landry mini <landry.buisson@gmail.com>
2023-10-28 00:07:53 +0200

    Découpage de la fonction scan_code en créant une noiuvelle fonction find_index_kid, qui ne sert qu'a trouver l'index de l'enfant dans le df a partir de son code scanné

--- logic.py
+++ logic.py
@@ -1,15 +1,23 @@
 import pandas as pd
 
 
+def find_index_kid(code, df:pd.DataFrame):
+    # Retourne l'index de l'enfant dans la dataframe
+    try:
+        index_enfant = df.loc[df["code"] == code].index.tolist()[0]
+    except IndexError:  # si le code lu est mauvais l,index de code ne se trouvera pas dans la df
+        index_enfant = None
+    return index_enfant
+
+
 def scan_code(code: str, df: pd.DataFrame, gala: str):
     # On initialise les valeurs par défaut en cas d'échec de lecture
     T1, T2, T3, T4 = '', '', '', ''
     other_g = ""
-    nom_enfant = 'Erreur de lecture'
-    try:
-        # on cherche l'index de la ligne correspondant au code de l'enfant
-        index_enfant = df.loc[df["code"] == code].index.tolist()[0]
-        # on récupère le nom et le prénom de l'enfant
+    # on cherche l'index de la ligne correspondant au code de l'enfant
+    index_enfant = find_index_kid(code, df)
+    # on récupère le nom et le prénom de l'enfant
+    if index_enfant != None:
         nom_enfant = df.at[index_enfant, "prénom"] + ' ' + df.at[index_enfant, 'nom']
         # on récupère la ou les chorées que l'enfant fait pendant ce Gala
         galas = [1, 2, 3]
@@ -36,6 +44,7 @@ def scan_code(code: str, df: pd.DataFrame, gala: str):
                     other_g = other_g + '❷'
                 elif col == 'Cours G3':
                     other_g = other_g + '❸'
-    except IndexError:  # si le code lu est mauvais l,index de code ne se trouvera pas dans la df
-        pass
-    return nom_enfant, T1, T2, T3, T4, other_g
+
+        return nom_enfant, T1, T2, T3, T4, other_g
+    else :
+        return 'Erreur de lecture', '', '', '', '', ''
